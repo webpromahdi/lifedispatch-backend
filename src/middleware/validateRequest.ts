9247fd09105c9f94bf5101230f8430d9fc41fb9d
Mahdi Al Hasan <138362772+webpromahdi@users.noreply.github.com>
2026-09-04 11:41:19 +0600

    feat: add core error handling and validation middleware with utilities

---
+++ src/middleware/validateRequest.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import type z from "zod";
+import { catchAsync } from "../utils/catchAsync.js";
+import { AppError } from "../utils/AppError.js";
+
+export const validateRequest = (zodSchema: z.ZodObject<any>) => {
+	return catchAsync((req: Request, res: Response, next: NextFunction) => {
+		const payload = req.body ?? {};
+		const result = zodSchema.safeParse(payload);
+
+		if (!result.success) {
+      const { error } = result;
+      console.log(error);
+      console.log(error.issues);
+      
+      const errorMessage = error.issues[0]?.message || "Validation error";
+      throw new AppError(httpStatus.BAD_REQUEST, errorMessage);
+    }
+
+		req.body = result.data;
+
+		next();
+	});
+};
